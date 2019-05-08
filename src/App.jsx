c80b7ac3fa1df41370913e1f988b23608358df59
Safdar Jamal <safdar.dev@gmail.com>
2019-05-08 21:30:14 +0500

    Convert func. to class component

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Crime Search App</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
