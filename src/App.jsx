f07ac9fcdc84a51169b603aedcd9b2e29044dd45
Safdar Jamal <safdar.dev@gmail.com>
2019-05-10 20:35:17 +0500

    Hello world with evergreen-ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import { Button } from 'evergreen-ui';
 
-class App extends React.Component {
+class App extends Component {
   constructor(props) {
     super(props);
 
@@ -10,10 +10,8 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Crime Search App</h1>
-        </header>
+      <div>
+        <Button>Hello World</Button>
       </div>
     );
   }
