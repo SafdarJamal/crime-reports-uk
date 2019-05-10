f077f965300ea4699e5bce1982e8d12c71271447
Safdar Jamal <safdar.dev@gmail.com>
2019-05-10 21:12:45 +0500

    Explored ui layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button } from 'evergreen-ui';
+import { Pane, Button, Heading, majorScale, BackButton } from 'evergreen-ui';
 
 class App extends Component {
   constructor(props) {
@@ -10,9 +10,24 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <Button>Hello World</Button>
-      </div>
+      <Pane display="flex" padding={16} background="tint2" borderRadius={3}>
+        <Pane flex={1} alignItems="center" display="flex">
+          <Heading size={600}>Left Aligned</Heading>
+        </Pane>
+        <Pane>
+          {/* Below you can see the marginRight property on a Button. */}
+          <Button marginRight={16} height={majorScale(5)}>
+            Button
+          </Button>
+          <Button marginRight={16} appearance="primary" intent="success">
+            Primary Button
+          </Button>
+          <Button appearance="primary" iconBefore="search">
+            Primary Button
+          </Button>
+          <BackButton marginLeft={16}>Back</BackButton>
+        </Pane>
+      </Pane>
     );
   }
 }
