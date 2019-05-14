16ba73207ea7055a7174660f0847b1fd0bf53479
Safdar Jamal <safdar.dev@gmail.com>
2019-05-14 19:35:23 +0500

    Change button color

--- src/components/PrimaryButton.jsx
+++ src/components/PrimaryButton.jsx
@@ -5,6 +5,7 @@ function PrimaryButton(props) {
   return (
     <Button
       appearance="primary"
+      intent="success"
       marginTop={25}
       height={majorScale(5)}
       iconBefore="search"
