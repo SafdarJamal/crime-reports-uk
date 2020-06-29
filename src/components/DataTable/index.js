a61e7c4c168f034a1d0ac3e5dc62db1cd9f2984d
Safdar Jamal <safdar.dev@gmail.com>
2020-06-29 19:42:02 +0500

    Convert DataTable  class component into functional component with hooks

--- src/components/DataTable/index.js
+++ src/components/DataTable/index.js
@@ -1,147 +1,123 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Table, Text, Pane, Spinner, Dialog } from 'evergreen-ui';
 import { copyToClipboard } from 'copy-lite';
 
-class DataTable extends Component {
-  constructor(props) {
-    super(props);
+const DataTable = ({ fetchingReports, reports }) => {
+  const [listNumber, setListNumber] = useState(15);
+  const [isBottom, setIsBottom] = useState(false);
+  const [isDialogShown, setIsDialogShown] = useState(false);
+  const [dialogID, setDialogID] = useState(null);
+  const [isCopied, setIsCopied] = useState(false);
 
-    this.state = {
-      listNumber: 15,
-      isBottom: false,
-      isDialogShown: false,
-      dialogID: null,
-      isCopied: false
-    };
-
-    this.handleScroll = this.handleScroll.bind(this);
-  }
-
-  handleScroll(event) {
+  const handleScroll = event => {
     const { scrollHeight, scrollTop, clientHeight } = event.target;
     const bottom = scrollHeight - scrollTop === clientHeight;
 
     if (bottom) {
-      const { listNumber, isBottom } = this.state;
-      const { reports } = this.props;
       const reportsArr = [...reports];
 
       if (reportsArr.length > listNumber) {
         if (!isBottom) {
-          this.setState({ isBottom: true });
+          setIsBottom(true);
         }
 
         setTimeout(() => {
-          this.setState({ listNumber: listNumber + 15 });
+          setListNumber(listNumber + 15);
         }, 1000);
       } else if (isBottom) {
-        this.setState({ isBottom: false });
+        setIsBottom(false);
       }
     }
+  };
+
+  let controlledList;
+
+  if (reports) {
+    controlledList = [...reports];
+    controlledList.length = listNumber;
+  } else {
+    controlledList = null;
   }
 
-  render() {
-    const {
-      listNumber,
-      isBottom,
-      isDialogShown,
-      dialogID,
-      isCopied
-    } = this.state;
-
-    const { fetchingReports, reports } = this.props;
-
-    let controlledList;
-
-    if (reports) {
-      controlledList = [...reports];
-      controlledList.length = listNumber;
-    } else {
-      controlledList = null;
-    }
-
-    return (
-      <Table
-        onScroll={this.handleScroll}
-        elevation={1}
-        marginTop={25}
-        marginBottom={25}
-        borderRadius={8}
+  return (
+    <Table
+      onScroll={handleScroll}
+      elevation={1}
+      marginTop={25}
+      marginBottom={25}
+      borderRadius={8}
+    >
+      <Dialog
+        isShown={isDialogShown}
+        hasHeader={false}
+        confirmLabel={isCopied ? 'Copied' : 'Copy'}
+        onConfirm={close => {
+          copyToClipboard(controlledList[dialogID].persistent_id);
+          setIsCopied(true);
+        }}
+        onCloseComplete={() => {
+          setIsDialogShown(false);
+          setIsCopied(false);
+        }}
       >
-        <Dialog
-          isShown={isDialogShown}
-          hasHeader={false}
-          confirmLabel={isCopied ? 'Copied' : 'Copy'}
-          onConfirm={close => {
-            copyToClipboard(controlledList[dialogID].persistent_id);
-            this.setState({ isCopied: true });
-          }}
-          onCloseComplete={() =>
-            this.setState({ isDialogShown: false, isCopied: false })
-          }
-        >
-          <Text>
-            <b>CRIME_ID:</b>
-            <br />
-            {isDialogShown && controlledList[dialogID].persistent_id}
-          </Text>
-        </Dialog>
+        <Text>
+          <b>CRIME_ID:</b>
+          <br />
+          {isDialogShown && controlledList[dialogID].persistent_id}
+        </Text>
+      </Dialog>
 
-        <Table.Head>
-          <Table.TextHeaderCell>NO.</Table.TextHeaderCell>
-          <Table.TextHeaderCell>ID</Table.TextHeaderCell>
-          <Table.TextHeaderCell>DATE</Table.TextHeaderCell>
-          <Table.TextHeaderCell>STATUS</Table.TextHeaderCell>
-        </Table.Head>
+      <Table.Head>
+        <Table.TextHeaderCell>NO.</Table.TextHeaderCell>
+        <Table.TextHeaderCell>ID</Table.TextHeaderCell>
+        <Table.TextHeaderCell>DATE</Table.TextHeaderCell>
+        <Table.TextHeaderCell>STATUS</Table.TextHeaderCell>
+      </Table.Head>
 
-        <Table.Body height={475}>
-          {fetchingReports && (
-            <Pane>
-              <Spinner size={50} marginX="auto" marginY={10} />
-            </Pane>
-          )}
+      <Table.Body height={475}>
+        {fetchingReports && (
+          <Pane>
+            <Spinner size={50} marginX="auto" marginY={10} />
+          </Pane>
+        )}
 
-          {!fetchingReports &&
-            controlledList &&
-            controlledList[0] === undefined && (
-              <Table.Row>
-                <Table.TextCell>
-                  <Text size={600} marginLeft={10}>
-                    There is no report available.
-                  </Text>
-                </Table.TextCell>
-              </Table.Row>
-            )}
+        {!fetchingReports && controlledList && controlledList[0] === undefined && (
+          <Table.Row>
+            <Table.TextCell>
+              <Text size={600} marginLeft={10}>
+                There is no report available.
+              </Text>
+            </Table.TextCell>
+          </Table.Row>
+        )}
 
-          {!fetchingReports &&
-            controlledList &&
-            controlledList[0] !== undefined &&
-            controlledList.map((report, i) => (
-              <Table.Row
-                key={i}
-                isSelectable
-                onSelect={() =>
-                  this.setState({ isDialogShown: true, dialogID: i })
-                }
-              >
-                <Table.TextCell>{i + 1}</Table.TextCell>
-                <Table.TextCell>{report.persistent_id}</Table.TextCell>
-                <Table.TextCell>{report.month}</Table.TextCell>
-                <Table.TextCell>
-                  {report.outcome_status.category}
-                </Table.TextCell>
-              </Table.Row>
-            ))}
+        {!fetchingReports &&
+          controlledList &&
+          controlledList[0] !== undefined &&
+          controlledList.map((report, i) => (
+            <Table.Row
+              key={i}
+              isSelectable
+              onSelect={() => {
+                setIsDialogShown(true);
+                setDialogID(i);
+              }}
+            >
+              <Table.TextCell>{i + 1}</Table.TextCell>
+              <Table.TextCell>{report.persistent_id}</Table.TextCell>
+              <Table.TextCell>{report.month}</Table.TextCell>
+              <Table.TextCell>{report.outcome_status.category}</Table.TextCell>
+            </Table.Row>
+          ))}
 
-          {isBottom && (
-            <Pane>
-              <Spinner size={50} marginX="auto" marginY={10} />
-            </Pane>
-          )}
-        </Table.Body>
-      </Table>
-    );
-  }
-}
+        {isBottom && (
+          <Pane>
+            <Spinner size={50} marginX="auto" marginY={10} />
+          </Pane>
+        )}
+      </Table.Body>
+    </Table>
+  );
+};
 
 export default DataTable;
