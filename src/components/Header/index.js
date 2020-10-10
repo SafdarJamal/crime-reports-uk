73cd27c696dd8c693d98748c6e3c4a79832794da
Safdar Jamal <safdar.dev@gmail.com>
2020-10-11 00:11:53 +0500

    Replace png logo with svg one

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Pane, Avatar, Heading } from 'evergreen-ui';
-import logo from '../../images/logo.png';
+import logo from '../../images/logo.svg';
 
 const Header = () => {
   return (
